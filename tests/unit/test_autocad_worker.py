9197790103d5c3b067a6ab320da25a5eada9de9d
sonicg <85807784@qq.com>
2026-08-25 16:38:05 +0800

    补强布局重命名顺序断言

--- tests/unit/test_autocad_worker.py
+++ tests/unit/test_autocad_worker.py
@@ -10,17 +10,25 @@ def test_render_rebuild_contains_handle_read_in_the_same_script():
             {
                 "source_file": "C:/sources/template.dwg",
                 "source_layout": "A3",
-                "target_layout": "001 平面",
-            }
+                "target_layout": "001-A",
+            },
+            {
+                "source_file": "C:/sources/template.dwg",
+                "source_layout": "A4",
+                "target_layout": "002-B",
+            },
         ],
     )
 
+    handles_index = script.index("DstGetLayoutHandles")
     assert script.count("_.NETLOAD") == 1
     assert script.count("DstGetLayoutHandles") == 1
     assert script.count("_.QSAVE") == 1
     assert script.count("_.QUIT") == 1
-    assert script.index("DstDeleteDefaultLayout") < script.index("DstGetLayoutHandles")
-    assert script.index("DstGetLayoutHandles") < script.index("_.QSAVE") < script.index("_.QUIT")
+    assert script.index("DstDeleteDefaultLayout") < handles_index
+    assert script.index("_.-LAYOUT\n_Rename\nDST_TMP_0000\n001-A") < handles_index
+    assert script.index("_.-LAYOUT\n_Rename\nDST_TMP_0001\n002-B") < handles_index
+    assert handles_index < script.index("_.QSAVE") < script.index("_.QUIT")
 
 
 def test_render_handles_remains_a_standalone_handle_script():
